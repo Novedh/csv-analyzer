e808b27b08754ad1ee6164990316ec7f96414072
novedH <devonhuang11@gmail.com>
2024-03-09 14:16:51 -0800

    linked list, working on part 2, display header done

--- assignment4.h
+++ assignment4.h
@@ -1,5 +1,126 @@
-
+/**************************************************************
+* Class::  CSC-415-01 Spring 2024
+* Name::Devon Huang
+* Student ID::916940666
+* GitHub-Name::Novedh
+* Project:: Assignment 4 – Processing CSV Data with Threads
+*
+* File:: assignment4.h
+*
+* Description:: A header file for the csvthreads project
+*
+**************************************************************/
 #define BUFFERSIZE 1024
+
+// for the times ill use an array of ints where each index will keep track of 
+// the time it takes 
+// 0's will keep track of 2 minutes or less
+// 1's will keep track of 3 to 5 minutes
+// 2's will keep track of 6 to 10 minute
+// 3's will keep track of over 10 minutes
+typedef struct EventData{
+    char *callType;
+    int totalCalls;
+    int dispatchTimes[4];
+    int onSceneTimes[4];
+    struct EventData *next;
+}EventData;
+
+EventData* createNode(char *callType, int totalCalls, int dispatchTimes[], int onSceneTimes[]) {
+    EventData* newNode = (EventData*)malloc(sizeof(EventData));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->callType = (char*)malloc(strlen(callType) + 1);
+    if (newNode->callType == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    strcpy(newNode->callType, callType);
+    newNode->totalCalls = totalCalls;
+    memcpy(newNode->dispatchTimes, dispatchTimes, sizeof(int) * 4);
+    memcpy(newNode->onSceneTimes, onSceneTimes, sizeof(int) * 4);
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertNode(EventData **head, EventData *newNode) {
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        EventData *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+int nodeCount(EventData *head){
+    int count =1;
+    while(head->next != NULL){
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+void swapNode(EventData *a, EventData *b) {
+    char *tempCallType = a->callType;
+    int tempTotalCalls = a->totalCalls;
+    int tempDispatchTimes[4];
+    int tempOnSceneTimes[4];
+    memcpy(tempDispatchTimes, a->dispatchTimes, sizeof(int) * 4);
+    memcpy(tempOnSceneTimes, a->onSceneTimes, sizeof(int) * 4);
+
+    a->callType = b->callType;
+    a->totalCalls = b->totalCalls;
+    memcpy(a->dispatchTimes, b->dispatchTimes, sizeof(int) * 4);
+    memcpy(a->onSceneTimes, b->onSceneTimes, sizeof(int) * 4);
+
+    b->callType = tempCallType;
+    b->totalCalls = tempTotalCalls;
+    memcpy(b->dispatchTimes, tempDispatchTimes, sizeof(int) * 4);
+    memcpy(b->onSceneTimes, tempOnSceneTimes, sizeof(int) * 4);
+}
+
+void sortNodes(EventData *head) {
+    if (head == NULL || head->next == NULL)
+        return;
+
+    int swapped;
+    EventData *ptr1;
+    EventData *lastptr = NULL;
+
+    while (1) {
+        swapped = 0;
+        ptr1 = head;
+
+        while (ptr1->next != lastptr) {
+            if (strcmp(ptr1->callType, ptr1->next->callType) > 0) {
+                swapNode(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        
+        if (!swapped){
+            break;
+        }
+
+        lastptr = ptr1;
+    }
+}
+
+void freeList(EventData *head) {
+    EventData *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp->callType);
+        free(temp);
+    }
+}
 
 char ** csvopen (char * filename);
 
--- huang_devon_HW4_main.c
+++ huang_devon_HW4_main.c
@@ -1,5 +1,5 @@
 /**************************************************************
-* Class::  CSC-415-0# Spring 2024
+* Class::  CSC-415-01 Spring 2024
 * Name::Devon Huang
 * Student ID::916940666
 * GitHub-Name::Novedh
@@ -17,13 +17,57 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #include <pthread.h>
+#include "assignment4.h"
 
 
 
 int main (int argc, char *argv[])
     {
     //***TO DO***  Look at arguments, initialize application
+    char ** header = csvopen (argv[1]);
+    int colms = 0;
+    while ((header[colms] != NULL) && (colms < 50)){
+        ++colms;
+    }
+	//Law100K.csv 1 police_district(26) BAYVIEW MISSION TENDERLOIN
+    //13 : call_type_final_desc
+    //10 : call_type_original_desc
+
+    //25 : analysis_neighborhood
+    //26 : police_district
+
+    //03 : received_datetime
+    //05 : dispatch_datetime
+    //06 : enroute_datetime
+    //07 : onscene_datetime
+
+    int threads = atoi(argv[2]);
+    char *subfield = argv[3];
+    char *subVal1 = argv[4];
+    char *subVal2 = argv[5];
+    char *subVal3 = argv[6];
+    char **line = csvnext();
+    int subfieldLoc = 0;
+
+    // this is to find which subfield the user choose, police district or neighborhood
+    for(int i =0; i<colms; i++){
+         if (strcmp(header[i], subfield) == 0) {
+            subfieldLoc = i;
+            break; 
+        }
+    }
+    // printf("\n\n%s",header[subfieldLoc]);
+    // for(int i =0; i<colms;i++){
+    //     printf("\n%02d : %s",i,header[i]);
+    // }
+    for(int i =0; i<colms;i++){
+        printf("\n%02d : %s",i,line[i]);
+    }
+    
+    
+    
 
     //**************************************************************
     // DO NOT CHANGE THIS BLOCK
@@ -36,9 +80,51 @@ int main (int argc, char *argv[])
     
     // *** TO DO ***  start your thread processing
     //                wait for the threads to finish
+    EventData *head = NULL;
+    char **currLine;
+    
+    while((currLine = csvnext() )!= NULL){
+        //if police district is argv[4], argv[5] or argv[6];
+        if(strcmp(currLine[subfieldLoc], subVal1) == 0 ){
+            
+
+        }
+        if(strcmp(currLine[subfieldLoc], subVal2) == 0 ){
+            
+        }
+        if(strcmp(currLine[subfieldLoc], subVal3) == 0 ){
+            
+        }
+        
+    }
+    printf("%d\n%d\n%d\n",count1,count2,count3);
+    // if((currLine[13] || currLine[10]) != ""){
+    //         printf("%s",currLine[13]);
+
+    //     }
+
 
 
     // ***TO DO *** Display Data
+    char * times =(" <2  | 3-5 | 6-10 | >10  ");
+    printf("%32s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|\n"
+    ," ","Total"," ","Total"," ",subVal1," ",subVal1," ",subVal2," ",subVal2," ",subVal3," ",
+    subVal3," ");
+    printf("%32s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|%20s%5s|\n",
+    " ","Dispatch Time"," ","On Scene Time"," ","Dispatch Time"," ","On Scene Time"," ",
+    "Dispatch Time"," ","On Scene Time"," ","Dispatch Time"," ","On Scene Time"," ");
+    printf("%23s|%8s|%25s|%25s|%25s|%25s|%25s|%25s|%25s|%25s|\n",
+    "Call type","Total",times,times,times,times,times,times,times,times);
+    for (int i = 0; i < 241; i++) {
+        printf("%c", '-');
+    }
+    printf("\n");
+
+    printf("%23s|%8s|%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|",
+    " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ");
+    printf("%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|%5s|%5s|%6s|%6s|\n"
+    ," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ");
+    
 
     //**************************************************************
     // DO NOT CHANGE THIS BLOCK
@@ -57,5 +143,8 @@ int main (int argc, char *argv[])
 
 
     // ***TO DO *** cleanup
+    csvclose();
+    
+    
     }
 
